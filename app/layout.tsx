2227cc7a8a7d930e2f06395cdc5cf7543b4ca7ba
Mukalazi Patrick <patricktwin1@gmail.com>
2026-03-17 20:03:49 +0300

    improved seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,6 @@ export const metadata: Metadata = {
   title: "Best Business Ideas in Uganda (2026) | Start with Low Capital",
   description:
     "Discover practical business ideas in Uganda you can start with low capital. Includes poultry, liquid soap, chapati, pig farming and more with step-by-step guides.",
- verification: {
-  google: "Y9d7o39Src2xSuPnkQs3a1SKd71MvXphThEY83L6-gI",
-},
     keywords: [
     "business ideas Uganda",
     "small business Uganda",
