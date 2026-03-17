04528e19b994222c931fe4ff8ecdfc16b03572ac
Mukalazi Patrick <patricktwin1@gmail.com>
2026-03-17 18:59:33 +0300

    improved homepage design and added featured sections

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://uganda-business-ideas.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://uganda-business-ideas.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://uganda-business-ideas.vercel.app/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://uganda-business-ideas.vercel.app/ideas/liquid-soap-business",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://uganda-business-ideas.vercel.app/ideas/poultry-farming",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://uganda-business-ideas.vercel.app/ideas/pig-farming",
+      lastModified: new Date(),
+    },
+  ];
+}
